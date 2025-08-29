c46bac51bdf4bb37ca9203ccd46f79c5b0be8dfc
MUSTAKIMBIN <mustakimbin24@gmail.com>
2025-08-29 16:57:33 +0600

    complited heart button functionality

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("object");
+let heartCount = parseInt(document.getElementById("heart-count").innerText);
+// console.log(heartCount);
+
+// heart button functionality
+const hearts = document.getElementsByClassName("heart-btn");
+for (let heart of hearts) {
+  heart.addEventListener("click", function () {
+    let newHeartCount = ++heartCount;
+    // console.log(newHeartCount);
+    document.getElementById("heart-count").innerText = newHeartCount;
+  });
+}
+
+// heart-btn
