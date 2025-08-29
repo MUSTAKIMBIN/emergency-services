5f7c0cda0729a99b97044eb00c0bb46b41cac829
MUSTAKIMBIN <mustakimbin24@gmail.com>
2025-08-29 23:32:08 +0600

    appilied copy count functionality

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 let heartCount = parseInt(document.getElementById("heart-count").innerText);
-// console.log(heartCount);
+let copyCount = parseInt(document.getElementById("copy-count").innerText);
 
 // heart button functionality
 const hearts = document.getElementsByClassName("heart-btn");
 for (let heart of hearts) {
   heart.addEventListener("click", function () {
     let newHeartCount = ++heartCount;
-    // console.log(newHeartCount);
     document.getElementById("heart-count").innerText = newHeartCount;
   });
 }
 
-// heart-btn
+// coppy btn functionality
+const copys = document.getElementsByClassName("copy-btn");
+for (let copy of copys) {
+  copy.addEventListener("click", function () {
+    console.log('" coppy"');
+    let totalCopyCount = ++copyCount;
+    document.getElementById("copy-count").innerText = totalCopyCount;
+  });
+}
